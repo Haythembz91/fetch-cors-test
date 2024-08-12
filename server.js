72f38b1caf72f6137b01c52ea20c5171dadeff6c
Haythembz91 <haylive8@gmail.com>
2024-08-12 23:34:08 +0100

    npm i cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const PORT = 8000;
 const express = require('express');
+const cors = require ('cors')
 const app = express();
+app.use(cors({
+  origin:'https://fetch-cors-test.vercel.app'
+}))
 
 app.get('/name', (req, res) => {
   res.json({ name: 'Ania Kubow' });
