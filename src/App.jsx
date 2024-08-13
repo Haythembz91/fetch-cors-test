d65c72926955c1048ae41b3c265dfc4629212170
Haythembz91 <154563842+Haythembz91@users.noreply.github.com>
2024-08-13 08:03:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState('');
   const getName = async () => {
     try {
-      const response = await fetch('http://localhost:8000/name');
+      const response = await fetch('name');
       if (response.status === 200) {
         const data = await response.json();
         setName(data.name);
