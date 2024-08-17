688477ff77b3304317c6de083998d2abb977340b
Haythembz91 <haylive8@gmail.com>
2024-08-17 15:10:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,11 @@ app.get('/name', (req, res) => {
   res.json({ name: 'Ania Kubow' });
 });
 
+
+app.get('/', (req, res) => {
+  res.json('Ania is the best');
+});
+
+
+
 app.listen(PORT, () => console.log('Server is running on Port: ' + PORT));
